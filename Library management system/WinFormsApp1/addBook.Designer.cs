eb2761b01325641382763f1dff01c295d2efb2c9
Ahsan <ahsansps.rwp@gmail.com>
2025-08-30 12:39:56 +0500

    add books

---
+++ Library management system/WinFormsApp1/addBook.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace Library_Management_System
+{
+    partial class addBook
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            dateTimePicker1 = new DateTimePicker();
+            textBox5 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            richTextBox1 = new RichTextBox();
+            textBox1 = new TextBox();
+            button2 = new Button();
+            button1 = new Button();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.Snow;
+            groupBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            groupBox1.Controls.Add(dateTimePicker1);
+            groupBox1.Controls.Add(textBox5);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(richTextBox1);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(54, 49);
+            groupBox1.Margin = new Padding(4, 5, 4, 5);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Padding = new Padding(4, 5, 4, 5);
+            groupBox1.Size = new Size(619, 726);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "add book details";
+            groupBox1.Enter += groupBox1_Enter;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(229, 271);
+            dateTimePicker1.Margin = new Padding(4, 5, 4, 5);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(265, 27);
+            dateTimePicker1.TabIndex = 14;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(229, 363);
+            textBox5.Margin = new Padding(4, 5, 4, 5);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(265, 27);
+            textBox5.TabIndex = 13;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(229, 206);
+            textBox3.Margin = new Padding(4, 5, 4, 5);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(265, 27);
+            textBox3.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(229, 146);
+            textBox2.Margin = new Padding(4, 5, 4, 5);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(265, 27);
+            textBox2.TabIndex = 10;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(229, 446);
+            richTextBox1.Margin = new Padding(4, 5, 4, 5);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(265, 136);
+            richTextBox1.TabIndex = 9;
+            richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(229, 74);
+            textBox1.Margin = new Padding(4, 5, 4, 5);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(265, 27);
+            textBox1.TabIndex = 8;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button2.Location = new Point(371, 625);
+            button2.Margin = new Padding(4, 5, 4, 5);
+            button2.Name = "button2";
+            button2.Size = new Size(123, 55);
+            button2.TabIndex = 7;
+            button2.Text = "Back";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            button1.Location = new Point(229, 625);
+            button1.Margin = new Padding(4, 5, 4, 5);
+            button1.Name = "button1";
+            button1.Size = new Size(127, 55);
+            button1.TabIndex = 6;
+            button1.Text = "Add Book";
+            button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.Location = new Point(49, 506);
+            label6.Margin = new Padding(4, 0, 4, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(104, 24);
+            label6.TabIndex = 5;
+            label6.Text = "Description";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.Location = new Point(49, 363);
+            label5.Margin = new Padding(4, 0, 4, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(68, 24);
+            label5.TabIndex = 4;
+            label5.Text = "Edition";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Location = new Point(49, 271);
+            label4.Margin = new Padding(4, 0, 4, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(137, 24);
+            label4.TabIndex = 3;
+            label4.Text = "Published Date";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(49, 209);
+            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(66, 24);
+            label3.TabIndex = 2;
+            label3.Text = "Author";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(49, 149);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(87, 24);
+            label2.TabIndex = 1;
+            label2.Text = "ISBN No:";
+            label2.Click += label2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft Sans Serif", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(49, 74);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(109, 24);
+            label1.TabIndex = 0;
+            label1.Text = "Book Name";
+            label1.Click += label1_Click;
+            // 
+            // addBook
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.PeachPuff;
+            ClientSize = new Size(737, 831);
+            Controls.Add(groupBox1);
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "addBook";
+            Text = "addBook";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
---
+++ Library management system/WinFormsApp1/addBook.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace Library_Management_System
+{
+    public partial class addBook : Form
+    {
+        public addBook()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Dashboard M = new Dashboard();
+            M.Show();
+            this.Hide();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Validation for Book Name (required)
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Book Name is required.");
+                return;
+            }
+
+            // Validation for ISBN (must be numeric and 10 or 13 digits)
+            if (string.IsNullOrWhiteSpace(textBox2.Text) || !textBox2.Text.All(char.IsDigit) || (textBox2.Text.Length != 10 && textBox2.Text.Length != 13))
+            {
+                MessageBox.Show("Please enter a valid ISBN (10 or 13 digits).");
+                return;
+            }
+
+            // Validation for Author (required)
+            if (string.IsNullOrWhiteSpace(textBox3.Text))
+            {
+                MessageBox.Show("Author is required.");
+                return;
+            }
+
+            // Validation for Published Date (must be a valid date)
+            if (dateTimePicker1.Value > DateTime.Now)
+            {
+                MessageBox.Show("Published Date cannot be in the future.");
+                return;
+            }
+
+            // Validation for Edition (optional, but must be numeric if entered)
+            if (!string.IsNullOrWhiteSpace(textBox5.Text) && !textBox5.Text.All(char.IsDigit))
+            {
+                MessageBox.Show("Edition must be a valid number if entered.");
+                return;
+            }
+
+            // Validation for Description (optional)
+            if (richTextBox1.Text.Length > 500)
+            {
+                MessageBox.Show("Description is too long. Please limit it to 500 characters.");
+                return;
+            }
+
+            // Generate a unique key for the book (e.g., a GUID)
+            string bookId = Guid.NewGuid().ToString();
+
+            // Create dictionary with form values
+            var bookData = new Dictionary<string, string>
+    {
+        { "BookName", textBox1.Text },
+        { "ISBN", textBox2.Text },
+        { "Author", textBox3.Text },
+        { "PublishedDate", dateTimePicker1.Value.ToString("yyyy-MM-dd") },
+        { "Edition", textBox5.Text },
+        { "Description", richTextBox1.Text }
+    };
+
+            string jsonFilePath = "E:\\coding\\Sem 4\\iteration 1 code\\WinFormsApp1\\WinFormsApp1\\books.json";
+
+            Dictionary<string, Dictionary<string, string>> booksDict;
+
+            // Load existing books if file exists
+            if (File.Exists(jsonFilePath))
+            {
+                string existingJson = File.ReadAllText(jsonFilePath);
+                booksDict = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, string>>>(existingJson)
+                            ?? new Dictionary<string, Dictionary<string, string>>();
+            }
+            else
+            {
+                booksDict = new Dictionary<string, Dictionary<string, string>>();
+            }
+
+            // Add new book with its unique ID
+            booksDict[bookId] = bookData;
+
+            // Save updated dictionary back to file
+            string updatedJson = JsonConvert.SerializeObject(booksDict, Formatting.Indented);
+            File.WriteAllText(jsonFilePath, updatedJson);
+
+            MessageBox.Show("Book added successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            ClearFormFields();
+        }
+
+
+
+        private void ClearFormFields()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox5.Clear();
+            richTextBox1.Clear();
+            dateTimePicker1.Value = DateTime.Now;
+        }
+
+    }
+}
