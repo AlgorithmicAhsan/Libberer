eeff80ea92188a3a3839dc9921a6672545dd903c
Ahsan <ahsansps.rwp@gmail.com>
2025-08-30 12:40:11 +0500

    add members

---
+++ Library management system/WinFormsApp1/addMember.Designer.cs
@@ -0,0 +1,290 @@
+﻿namespace Library_Management_System
+{
+    partial class addMember
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            button2 = new Button();
+            button1 = new Button();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            dateTimePicker1 = new DateTimePicker();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            richTextBox1 = new RichTextBox();
+            textBox1 = new TextBox();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.WhiteSmoke;
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Controls.Add(dateTimePicker1);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(richTextBox1);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(56, 45);
+            groupBox1.Margin = new Padding(4, 5, 4, 5);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Padding = new Padding(4, 5, 4, 5);
+            groupBox1.Size = new Size(569, 666);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Add Member Details";
+            // 
+            // button2
+            // 
+            button2.BackColor = SystemColors.ActiveCaption;
+            button2.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = SystemColors.ActiveCaptionText;
+            button2.Location = new Point(336, 585);
+            button2.Margin = new Padding(4, 5, 4, 5);
+            button2.Name = "button2";
+            button2.Size = new Size(100, 49);
+            button2.TabIndex = 16;
+            button2.Text = "Back";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(171, 585);
+            button1.Margin = new Padding(4, 5, 4, 5);
+            button1.Name = "button1";
+            button1.Size = new Size(131, 49);
+            button1.TabIndex = 15;
+            button1.Text = "Add member";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Location = new Point(309, 326);
+            radioButton2.Margin = new Padding(4, 5, 4, 5);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(78, 24);
+            radioButton2.TabIndex = 14;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Female";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Location = new Point(185, 326);
+            radioButton1.Margin = new Padding(4, 5, 4, 5);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(63, 24);
+            radioButton1.TabIndex = 13;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Male";
+            radioButton1.UseVisualStyleBackColor = true;
+            radioButton1.CheckedChanged += radioButton1_CheckedChanged;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(171, 254);
+            dateTimePicker1.Margin = new Padding(4, 5, 4, 5);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(265, 27);
+            dateTimePicker1.TabIndex = 12;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(171, 489);
+            textBox4.Margin = new Padding(4, 5, 4, 5);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(264, 27);
+            textBox4.TabIndex = 11;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(171, 435);
+            textBox3.Margin = new Padding(4, 5, 4, 5);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(265, 27);
+            textBox3.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(169, 375);
+            textBox2.Margin = new Padding(4, 5, 4, 5);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(265, 27);
+            textBox2.TabIndex = 9;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(171, 145);
+            richTextBox1.Margin = new Padding(4, 5, 4, 5);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(264, 78);
+            richTextBox1.TabIndex = 8;
+            richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(169, 88);
+            textBox1.Margin = new Padding(4, 5, 4, 5);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(265, 27);
+            textBox1.TabIndex = 7;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(35, 500);
+            label7.Margin = new Padding(4, 0, 4, 0);
+            label7.Name = "label7";
+            label7.Size = new Size(85, 20);
+            label7.TabIndex = 6;
+            label7.Text = "Occupation";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(45, 435);
+            label6.Margin = new Padding(4, 0, 4, 0);
+            label6.Name = "label6";
+            label6.Size = new Size(107, 20);
+            label6.TabIndex = 5;
+            label6.Text = "E-mail address";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(45, 375);
+            label5.Margin = new Padding(4, 0, 4, 0);
+            label5.Name = "label5";
+            label5.Size = new Size(105, 20);
+            label5.TabIndex = 4;
+            label5.Text = "Phone number";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(48, 326);
+            label4.Margin = new Padding(4, 0, 4, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(57, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Gender";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(48, 263);
+            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(40, 20);
+            label3.TabIndex = 2;
+            label3.Text = "DOB";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(48, 149);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(62, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Address";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(48, 88);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(73, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Full name";
+            label1.Click += label1_Click;
+            // 
+            // addMember
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.PeachPuff;
+            ClientSize = new Size(700, 769);
+            Controls.Add(groupBox1);
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "addMember";
+            Text = "addMember";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ Library management system/WinFormsApp1/addMember.cs
@@ -0,0 +1,165 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Library_Management_System
+{
+    public partial class addMember : Form
+    {
+        public addMember()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Dashboard B = new Dashboard();
+            B.Show();
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Validation for Full Name (required)
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                MessageBox.Show("Full Name is required.");
+                return;
+            }
+
+            // Validation for Address (required)
+            if (string.IsNullOrWhiteSpace(richTextBox1.Text))
+            {
+                MessageBox.Show("Address is required.");
+                return;
+            }
+
+            // Validation for DOB (must be a valid date and not in the future)
+            if (dateTimePicker1.Value > DateTime.Now)
+            {
+                MessageBox.Show("DOB cannot be in the future.");
+                return;
+            }
+
+            // Validation for Gender (must be selected)
+            if (!radioButton1.Checked && !radioButton2.Checked)
+            {
+                MessageBox.Show("Please select a gender.");
+                return;
+            }
+
+            // Validation for Phone Number (required and numeric)
+            if (string.IsNullOrWhiteSpace(textBox2.Text) || !textBox2.Text.All(char.IsDigit))
+            {
+                MessageBox.Show("Phone number is required and must be numeric.");
+                return;
+            }
+
+            // Validation for Email (must be in a valid email format)
+            if (string.IsNullOrWhiteSpace(textBox3.Text) || !IsValidEmail(textBox3.Text))
+            {
+                MessageBox.Show("Please enter a valid email address.");
+                return;
+            }
+
+            // Validation for Occupation (optional, but if entered, must be a valid string)
+            if (!string.IsNullOrWhiteSpace(textBox4.Text) && textBox4.Text.Length > 100)
+            {
+                MessageBox.Show("Occupation description is too long. Please limit it to 100 characters.");
+                return;
+            }
+
+            // Determine selected gender
+            string gender = radioButton1.Checked ? "Male" :
+                            radioButton2.Checked ? "Female" : "Unspecified";
+
+            // Create a dictionary to hold member data
+            var memberData = new Dictionary<string, string>
+    {
+        { "FullName", textBox1.Text },
+        { "Address", richTextBox1.Text },
+        { "DOB", dateTimePicker1.Value.ToString("yyyy-MM-dd") },
+        { "Gender", gender },
+        { "Phone", textBox2.Text },
+        { "Email", textBox3.Text },
+        { "Occupation", textBox4.Text }
+    };
+
+            // Generate unique Member ID
+            string memberId = Guid.NewGuid().ToString();
+
+            // Define path for JSON file
+            string jsonFilePath = "E:\\coding\\Sem 4\\iteration 1 code\\WinFormsApp1\\WinFormsApp1\\members.json";
+
+            // Load existing members
+            Dictionary<string, Dictionary<string, string>> membersDict;
+            if (File.Exists(jsonFilePath))
+            {
+                string existingJson = File.ReadAllText(jsonFilePath);
+                membersDict = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, string>>>(existingJson)
+                              ?? new Dictionary<string, Dictionary<string, string>>();
+            }
+            else
+            {
+                membersDict = new Dictionary<string, Dictionary<string, string>>();
+            }
+
+            // Add new member to the dictionary
+            membersDict[memberId] = memberData;
+
+            // Write updated data to file
+            string updatedJson = JsonConvert.SerializeObject(membersDict, Formatting.Indented);
+            File.WriteAllText(jsonFilePath, updatedJson);
+
+            // Show confirmation
+            MessageBox.Show("Member added successfully!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            // Optional: clear form
+            ClearFormFields();
+        }
+
+        // Helper method to validate email format
+        private bool IsValidEmail(string email)
+        {
+            try
+            {
+                var mail = new System.Net.Mail.MailAddress(email);
+                return mail.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private void ClearFormFields()
+        {
+            textBox1.Clear();
+            richTextBox1.Clear();
+            dateTimePicker1.Value = DateTime.Now;
+            radioButton1.Checked = false;
+            radioButton2.Checked = false;
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+        }
+
+    }
+}
