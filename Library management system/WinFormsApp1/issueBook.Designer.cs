e28e705e72ddd27f6653362503cb5e8fb5c2a46d
Ahsan <ahsansps.rwp@gmail.com>
2025-08-30 12:41:01 +0500

    issue book

---
+++ Library management system/WinFormsApp1/issueBook.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace Library_Management_System
+{
+    partial class issueBook
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            groupBox1 = new GroupBox();
+            button2 = new Button();
+            button1 = new Button();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            groupBox1.BackColor = Color.Snow;
+            groupBox1.Controls.Add(button2);
+            groupBox1.Controls.Add(button1);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(textBox1);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Font = new Font("Courier New", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            groupBox1.Location = new Point(59, 40);
+            groupBox1.Margin = new Padding(4, 5, 4, 5);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Padding = new Padding(4, 5, 4, 5);
+            groupBox1.Size = new Size(565, 540);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Issue books details";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(197, 380);
+            button2.Margin = new Padding(4, 5, 4, 5);
+            button2.Name = "button2";
+            button2.Size = new Size(107, 48);
+            button2.TabIndex = 9;
+            button2.Text = "Ok";
+            button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            button1.Location = new Point(364, 380);
+            button1.Margin = new Padding(4, 5, 4, 5);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 48);
+            button1.TabIndex = 8;
+            button1.Text = "Back";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(311, 263);
+            textBox4.Margin = new Padding(4, 5, 4, 5);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(217, 29);
+            textBox4.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(311, 191);
+            textBox3.Margin = new Padding(4, 5, 4, 5);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(217, 29);
+            textBox3.TabIndex = 6;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(311, 135);
+            textBox2.Margin = new Padding(4, 5, 4, 5);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(217, 29);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(311, 71);
+            textBox1.Margin = new Padding(4, 5, 4, 5);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(217, 29);
+            textBox1.TabIndex = 4;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(68, 274);
+            label4.Margin = new Padding(4, 0, 4, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(131, 21);
+            label4.TabIndex = 3;
+            label4.Text = "Issued date";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(68, 195);
+            label3.Margin = new Padding(4, 0, 4, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(76, 21);
+            label3.TabIndex = 2;
+            label3.Text = "Author";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(68, 140);
+            label2.Margin = new Padding(4, 0, 4, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(131, 21);
+            label2.TabIndex = 1;
+            label2.Text = "ISBN number";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(68, 75);
+            label1.Margin = new Padding(4, 0, 4, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(186, 21);
+            label1.TabIndex = 0;
+            label1.Text = "Issued book name";
+            // 
+            // issueBook
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.PeachPuff;
+            ClientSize = new Size(721, 626);
+            Controls.Add(groupBox1);
+            ForeColor = SystemColors.InfoText;
+            Margin = new Padding(4, 5, 4, 5);
+            Name = "issueBook";
+            Text = "issueBook";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Library management system/WinFormsApp1/issueBook.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+
+namespace Library_Management_System
+{
+    public partial class issueBook : Form
+    {
+        public issueBook()
+        {
+            InitializeComponent();
+            button2.Click += button2_Click; // Hook up OK button
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Dashboard c = new Dashboard();
+            c.Show();
+            this.Hide();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Step 1: Validate fields
+            if (string.IsNullOrWhiteSpace(textBox1.Text) ||
+                string.IsNullOrWhiteSpace(textBox2.Text) ||
+                string.IsNullOrWhiteSpace(textBox3.Text) ||
+                string.IsNullOrWhiteSpace(textBox4.Text))
+            {
+                MessageBox.Show("Please fill in all fields.", "Validation Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string bookName = textBox1.Text.Trim();
+            string isbn = textBox2.Text.Trim();
+            string author = textBox3.Text.Trim();
+            string issueDate = textBox4.Text.Trim();
+
+            string booksFile = "E:\\coding\\Sem 4\\iteration 1 code\\WinFormsApp1\\WinFormsApp1\\books.json";
+            string issuedBooksFile = "E:\\coding\\Sem 4\\iteration 1 code\\WinFormsApp1\\WinFormsApp1\\issuedbooks.json";
+
+            if (!File.Exists(booksFile))
+            {
+                MessageBox.Show("books.json not found.");
+                return;
+            }
+
+            // Step 2: Load books.json
+            var booksData = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, string>>>(File.ReadAllText(booksFile));
+
+            // Step 3: Try to find a matching book
+            var matchingBook = booksData.FirstOrDefault(b =>
+                b.Value.TryGetValue("BookName", out var bn) && bn == bookName &&
+                b.Value.TryGetValue("ISBN", out var isb) && isb == isbn &&
+                b.Value.TryGetValue("Author", out var auth) && auth == author
+            );
+
+            if (matchingBook.Key == null)
+            {
+                MessageBox.Show("Book not found in library.", "Not Found", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            // Step 4: Load issuedbooks.json
+            Dictionary<string, Dictionary<string, string>> issuedBooksData = new();
+            if (File.Exists(issuedBooksFile))
+            {
+                issuedBooksData = JsonConvert.DeserializeObject<Dictionary<string, Dictionary<string, string>>>(File.ReadAllText(issuedBooksFile));
+            }
+
+            // Step 5: Move book to issuedbooks.json
+            issuedBooksData[matchingBook.Key] = matchingBook.Value;
+            booksData.Remove(matchingBook.Key);
+
+            File.WriteAllText(booksFile, JsonConvert.SerializeObject(booksData, Formatting.Indented));
+            File.WriteAllText(issuedBooksFile, JsonConvert.SerializeObject(issuedBooksData, Formatting.Indented));
+
+            MessageBox.Show("Book successfully issued!", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            ClearFormFields();
+        }
+
+        private void ClearFormFields()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+        }
+    }
+}
